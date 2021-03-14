2480582d75fba82c8bc4c0ed12ba3700ccebf6fb
Sammy Antigua <samiami83@hotmail.com>
2021-03-14 12:32:22 -0400

    removed all commented lines, made ready for final push

--- js/app.js
+++ js/app.js
@@ -1,181 +1,181 @@
-"use strict";
+'use strict';
 let rightAns = 0;
 let userName = prompt('What is your name?');
-alert('Hello ' + userName + ", it is a pleasure to have you here today.");
+alert('Hello ' + userName + ', it is a pleasure to have you here today.');
 
-// question 1
+
 function bornHere () {
   let bornHere = prompt('Was I born on a planet other than Earth? (y/n)').toLowerCase();
 
   if(bornHere === 'y') {
-    // console.log('Incorrect, I was born here on Earth.');
+
     alert('Incorrect, I was born here on Earth.');
-} else if (bornHere === 'yes') {
-    // console.log('Incorrect, I was born here on Earth.');
+  } else if (bornHere === 'yes') {
+
     alert('Incorrect, I was born here on Earth.');
-} else if (bornHere === 'n') {
-    // console.log('Correct, I am as Earthling as they come.');
+  } else if (bornHere === 'n') {
+
     alert('Correct, I am as Earthling as they come.');
     rightAns = (rightAns + 1);
-} else if (bornHere === 'no') {
-    // console.log('Correct, I am as Earthling as they come.');
+  } else if (bornHere === 'no') {
+
     alert('Correct, I am as Earthling as they come.');
     rightAns = (rightAns + 1);
-} else {
-    alert('Yes or no answers only please.')
+  } else {
+    alert('Yes or no answers only please.');
+  }
 }
-};
 
 bornHere();
 
 function myServ (){
 
-let myServ = prompt('Did I serve in the Space Force? (y/n)').toLowerCase();
+  let myServ = prompt('Did I serve in the Space Force? (y/n)').toLowerCase();
 
-if(myServ === 'y') {
-    // console.log('Wrong, the Space Force was not around then.');
+  if(myServ === 'y') {
+
     alert('Wrong, the Space Force was not around then.');
-} else if (myServ === 'yes') {
-    // console.log('Wrong, the Space Force was not around then.');
+  } else if (myServ === 'yes') {
+
     alert('Wrong, the Space Force was not around then.');
-} else if (myServ === 'n') {
-    // console.log('Correct, I served in the Navy.');
+  } else if (myServ === 'n') {
+
     alert('Correct, I served in the Navy.');
     rightAns = (rightAns + 1);
-} else if (myServ === 'no') {
-    // console.log('Correct, I served in the Navy.');
+  } else if (myServ === 'no') {
+
     alert('Correct, I served in the Navy.');
     rightAns = (rightAns + 1);
-} else {
-    alert('Yes or no answers only please.')
+  } else {
+    alert('Yes or no answers only please.');
+  }
 }
-};
 
 myServ();
 
 function myJob() {
 
-let myJob = prompt('Did I pursue a career in the music industry? (y/n)').toLowerCase();
+  let myJob = prompt('Did I pursue a career in the music industry? (y/n)').toLowerCase();
 
-if(myJob === 'y') {
-    // console.log('Incorrect, I went back to working as a technician.');
+  if(myJob === 'y') {
+
     alert('Incorrect, I went back to working as a technician.');
-} else if (myJob === 'yes') {
-    // console.log('Incorrect, I went back to working as a technician.');
+  } else if (myJob === 'yes') {
+
     alert('Incorrect, I went back to working as a technician.');
-} else if (myJob === 'n') {
-    // console.log('Correct, it was not for me.');
+  } else if (myJob === 'n') {
+
     alert('Correct, it was not for me.');
     rightAns = (rightAns + 1);
-} else if (myJob === 'no') {
-    // console.log('Correct, it was not for me.');
+  } else if (myJob === 'no') {
+
     alert('Correct, it was not for me.');
     rightAns = (rightAns + 1);
-} else {
-    alert('Yes or no answers only please.')
+  } else {
+    alert('Yes or no answers only please.');
+  }
 }
-};
 
 myJob();
 
 function gradSchool () {
 
-let gradSchool = prompt('Did I finish high-school? (y/n)').toLowerCase();
+  let gradSchool = prompt('Did I finish high-school? (y/n)').toLowerCase();
 
-if(gradSchool === 'y') {
-    // console.log('Indeed I did, right at the turn of the century!');
+  if(gradSchool === 'y') {
+
     alert('Indeed I did, right at the turn of the century!');
     rightAns = (rightAns + 1);
-} else if (gradSchool === 'yes') {
-    // console.log('Indeed I did, right at the turn of the century!');
+  } else if (gradSchool === 'yes') {
+
     alert('Indeed I did, right at the turn of the century!');
     rightAns = (rightAns + 1);
-} else if (gradSchool === 'n') {
-    // console.log('Incorrect. It was touch and go for a while but I did manage to earn my diploma.');
+  } else if (gradSchool === 'n') {
+
     alert('Incorrect. It was touch and go for a while but I did manage to earn my diploma.');
-} else if (gradSchool === 'no') {
-    // console.log('Incorrect. It was touch and go for a while but I did manage to earn my diploma.');
+  } else if (gradSchool === 'no') {
+
     alert('Incorrect. It was touch and go for a while but I did manage to earn my diploma.');
-} else {
-    alert('Yes or no answers only please.')
+  } else {
+    alert('Yes or no answers only please.');
+  }
 }
-};
 
 gradSchool();
 
 function myGoals () {
 
-let myGoals = prompt('Do I have any goals for my future? (y/n)').toLowerCase();
+  let myGoals = prompt('Do I have any goals for my future? (y/n)').toLowerCase();
 
-if(myGoals === 'y') {
-    // console.log('Correct, I intend to break stuff with my code.');
+  if(myGoals === 'y') {
+
     alert('Correct, I intend to break stuff with my code.');
     rightAns = (rightAns + 1);
-} else if (myGoals === 'yes') {
-    // console.log('Correct, I intend to break stuff with my code.');
+  } else if (myGoals === 'yes') {
+
     alert('Correct, I intend to break stuff with my code.');
     rightAns = (rightAns + 1);
-} else if (myGoals === 'n') {
-    // console.log('Wrong, I have many things I hope to achieve.');
+  } else if (myGoals === 'n') {
+
     alert('Wrong, I have many things I hope to achieve.');
-} else if (myGoals === 'no') {
-    // console.log('Wrong, I have many things I hope to achieve.');
+  } else if (myGoals === 'no') {
+
     alert('Wrong, I have many things I hope to achieve.');
-} else {
-    alert('Yes or no answers only please.')
+  } else {
+    alert('Yes or no answers only please.');
+  }
 }
-};
 
 myGoals();
 
 
-// Number Game
+
 
 function numberGame () {
-let counter = 0;
-while(counter < 4) {
+  let counter = 0;
+  while(counter < 4) {
     let guessNum = parseInt(prompt('Now, can you please guess a number 1-10?'));
     if (guessNum < 7) {
-        alert('No. That is too low. Try again.');
-        counter += 1;
+      alert('No. That is too low. Try again.');
+      counter += 1;
     } else if (guessNum > 7) {
-        alert('No. That is too high. Try again.');
-        counter += 1;
+      alert('No. That is too high. Try again.');
+      counter += 1;
     } else if (guessNum === 7) {
-        alert('You got it! Lucky number 7!!!');
-        rightAns = (rightAns + 1);
-        break;
+      alert('You got it! Lucky number 7!!!');
+      rightAns = (rightAns + 1);
+      break;
     }
     if (counter === 4) {
-        alert('You have exhausted your guesses, sorry.');
+      alert('You have exhausted your guesses, sorry.');
     }
+  }
 }
-};
 numberGame();
 
-// Second Number Game
+
 
 function numberGame2 () {
-let pets = ["fish",'snake','turtle','cat','dog','rabbit','zebra','armadillo','orangutan'];
-for(let i = 0;i < 6;i++) {
+  let pets = ['fish','snake','turtle','cat','dog','rabbit','zebra','armadillo','orangutan'];
+  for(let i = 0;i < 6;i++) {
     let guessPet = prompt('What kind of animal makes for a good pet?').toLowerCase();
-    let correct = false
+    let correct = false;
     for(let j = 0;j < pets.length;j++){
-        
-        if (guessPet === pets[j]) {
-            alert('That is a great choice!');
-            correct = true
-            rightAns = (rightAns + 1);
-            break;
-        }
+
+      if (guessPet === pets[j]) {
+        alert('That is a great choice!');
+        correct = true;
+        rightAns = (rightAns + 1);
+        break;
+      }
     }
     if (correct === true) {
-        break;
+      break;
     }
     alert('Sorry, guess again.');
+  }
+  alert('Possible correct answers include ' + pets);
 }
-alert('Possible correct answers include ' + pets);
-};
 
 numberGame2();
 
